8624aab8fff3493716ab7f7814963a9307d9b843
Thanusha204 <153809655+Thanusha204@users.noreply.github.com>
2025-08-01 20:42:16 +0530

    Add files via upload

---
+++ apex/script.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const alertButton = document.getElementById('alertButton');
+
+// Add a click event listener to the button
+alertButton.addEventListener('click', function() {
+    // This function will execute when the button is clicked
+    alert('Welcome to Karnataka! We hope you have an amazing trip.');
+});
