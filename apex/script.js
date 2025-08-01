fd50d6bcb5f7fab7f7296d988f8c6e00109f9bda
Thanusha204 <153809655+Thanusha204@users.noreply.github.com>
2025-08-01 20:52:53 +0530

    Delete apex directory

--- apex/script.js
+++
@@ -1,8 +0,0 @@
-// Get the button element by its ID
-const alertButton = document.getElementById('alertButton');
-
-// Add a click event listener to the button
-alertButton.addEventListener('click', function() {
-    // This function will execute when the button is clicked
-    alert('Welcome to Karnataka! We hope you have an amazing trip.');
-});
